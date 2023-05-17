e6630a3d36a0fb56cc099d3779eb6859fe2f5d63
David R. Myers <davidrmyersii@gmail.com>
2023-05-17 18:52:16 -0400

    Allow top-level arrow functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': ['off', {}],
     '@typescript-eslint/quotes': ['error', 'single', { allowTemplateLiterals: true }],
     'antfu/if-newline': ['off', {}],
+    'antfu/top-level-function': ['off', {}],
     'arrow-parens': ['off', {}],
     'curly': ['off', {}],
   },
